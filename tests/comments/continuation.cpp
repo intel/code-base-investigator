a7dff7e93401f8da5f2c4a868fb96175b5d2b377
Jason Sewall <jason.sewall@intel.com>
2019-11-05 10:22:39 -0800

    Add test for complex C code with continuations

---
+++ tests/comments/continuation.cpp
@@ -0,0 +1,55 @@
+// Copyright (C) 2019-2020 Intel Corporation
+// SPDX-License-Identifier: BSD-3-Clause
+
+int i = \
+    5; // comment \
+         lines \
+         more
+
+
+int x = \
+    1 /* now what \
+        comment \
+       // \
+       */ +2/\
+* hahaha */+3;
+
+"long //   - - -   string \
+and    \
+\
+\
+stuff    "
+
+  \     char w[] =
+"confusing \"\
+   string \" \n\
+\"  \" \\ \/ /* \* */ "; \
+"long   - - -   string \
+and /* \" */           \
+   \
+   \
+stuff    "
+
+/* big block comment
+** and so on
+** and so on too
+*/
+
+'"'
+
+''//what about this?\
+d'
+
+'/'
+
+"'\"'"
+
+int foo(); /\
+* hahahaha *\
+/
+
+#warning Dangerous don't do this
+#warning "This is more safe"
+
+/* "Strings 'r' // Fun! *\
+/
---
+++ tests/comments/test_comments.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2019 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+
+import unittest
+import logging
+import os
+from codebasin import preprocessor, file_parser
+
+class TestExampleFile(unittest.TestCase):
+    """
+    Test handling of comments
+    """
+
+    def test_c_comments(self):
+        rootdir = "./tests/comments/"
+        parser = file_parser.FileParser(os.path.join(rootdir, "continuation.cpp"))
+
+        tree = parser.parse_file()
+        self.assertEqual(tree.root.children[0].num_lines, 25)
+
+if __name__ == '__main__':
+    unittest.main()
