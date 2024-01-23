fadae33c4de1c00a3f978556dbd40689b81acc75
John Pennycook <john.pennycook@intel.com>
2024-01-23 14:34:05 -0800

    Fix formatting of setup.py
    Signed-off-by: John Pennycook <john.pennycook@intel.com>

--- setup.py
+++ setup.py
@@ -4,25 +4,30 @@
 
 from setuptools import setup
 
-setup(name='codebasin',
-      version='1.1.0',
-      description='Code Base Investigator',
-      author='John Pennycook',
-      author_email='john.pennycook@intel.com',
-      url='https://www.github.com/intel/code-base-investigator',
-      packages=['codebasin', 'codebasin.schema', 'codebasin.walkers'],
-      include_package_data=True,
-      scripts=['codebasin.py'],
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Environment :: Console',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Programming Language :: Python',
-                   'Topic :: Software Development'],
-      python_requires='>=3.9',
-      install_requires=['numpy',
-                        'matplotlib',
-                        'pyyaml',
-                        'scipy>=1.11.1',
-                        'jsonschema']
-      )
+setup(
+    name="codebasin",
+    version="1.1.0",
+    description="Code Base Investigator",
+    author="John Pennycook",
+    author_email="john.pennycook@intel.com",
+    url="https://www.github.com/intel/code-base-investigator",
+    packages=["codebasin", "codebasin.schema", "codebasin.walkers"],
+    include_package_data=True,
+    scripts=["codebasin.py"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "pyyaml",
+        "scipy>=1.11.1",
+        "jsonschema",
+    ],
+)
