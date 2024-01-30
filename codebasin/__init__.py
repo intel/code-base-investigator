cdf2261ba6e88f88a10a80628f6b0348ce4299ba
John Pennycook <john.pennycook@intel.com>
2024-01-30 07:40:08 -0800

    Deprecate the codebasin package
    The codebasin package was not originally designed for downstream users:
    the interface is undocumented, and in many cases is not very user-friendly.
    
    Semantic versioning recommends that there be at least one minor release with a
    deprecation warning prior to removing functionality in a major release.
    Deprecating the entire package is a simple solution that accurately
    communicates our intent to users.
    
    Signed-off-by: John Pennycook <john.pennycook@intel.com>

--- codebasin/__init__.py
+++ codebasin/__init__.py
@@ -1,3 +1,13 @@
 # Copyright (C) 2019-2024 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
+import warnings
+
 import codebasin.walkers
+
+warnings.warn(
+    "Calling codebasin package internals is deprecated. "
+    + "Please call the codebasin script directly instead. "
+    + "A new, stable, package interface will be introduced in "
+    + "a future release of Code Base Investigator.",
+    DeprecationWarning,
+)
