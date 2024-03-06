84665d70106e102250f7424b47f15be0e6c9a179
John Pennycook <john.pennycook@intel.com>
2024-03-06 11:57:55 -0800

    Test that duplicate files count towards divergence
    This replaces the old "duplicates" test, which was wrong.
    
    Duplicates SHOULD count towards divergence by default. Assuming that duplicates
    only arise due to out-of-tree builds is incorrect.
    
    Signed-off-by: John Pennycook <john.pennycook@intel.com>

--- tests/duplicates/build1/bar.h
+++
@@ -1,5 +0,0 @@
-/*
- * Copyright (C) 2021 Intel Corporation
- * SPDX-License-Identifier: BSD-3-Clause
- */
-uint32_t bar(uint32_t);
--- tests/duplicates/build1/foo.h
+++
@@ -1,5 +0,0 @@
-/*
- * Copyright (C) 2021 Intel Corporation
- * SPDX-License-Identifier: BSD-3-Clause
- */
-int foo(int);
