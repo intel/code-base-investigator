e4ca7a9a4d39694bc35551d18ccdb0f515695747
John Pennycook <john.pennycook@intel.com>
2024-03-18 08:29:24 -0700

    Add CompileCommand class
    Represents a single compile command from a compilation database.
    
    Signed-off-by: John Pennycook <john.pennycook@intel.com>

--- codebasin/__init__.py
+++ codebasin/__init__.py
@@ -1,7 +1,9 @@
 # Copyright (C) 2019-2024 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
+import shlex
 import warnings
 
+import codebasin.source
 import codebasin.walkers
 
 warnings.warn(
@@ -11,3 +13,114 @@ warnings.warn(
     + "a future release of Code Base Investigator.",
     DeprecationWarning,
 )
+
+
+class CompileCommand:
+    """
+    A single compile command from a compilation database.
+
+    Attributes
+    ----------
+    filename: string
+        The name of the source file compiled by this command.
+
+    directory: string, optional
+        The working directory for this command.
+
+    arguments: list[string], optional
+        The `argv` for this command, including the executable as `argv[0]`.
+
+    output: string, optional
+        The name of the file produced by this command, or None if not
+        specified.
+    """
+
+    def __init__(
+        self,
+        filename,
+        directory=None,
+        arguments=None,
+        command=None,
+        output=None,
+    ):
+        """
+        Raises
+        ------
+        ValueError
+            If both arguments and command are None.
+        """
+        self._filename = filename
+        self._directory = directory
+        if arguments is None and command is None:
+            raise ValueError("CompileCommand requires arguments or command.")
+        self._arguments = arguments
+        self._command = command
+        self._output = output
+
+    @property
+    def directory(self):
+        return self._directory
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @property
+    def arguments(self):
+        if self._arguments is None:
+            return shlex.split(self._command)
+        else:
+            return self._arguments
+
+    @property
+    def output(self):
+        return self._output
+
+    def __str__(self):
+        if self._command is None:
+            return " ".join(self._arguments)
+        else:
+            return self._command
+
+    def is_supported(self):
+        """
+        Returns
+        -------
+        bool
+            True if the command can be emulated and False otherwise.
+            Commands that are not supported will not impact analysis.
+        """
+        # Empty commands don't do anything
+        if len(self.arguments) == 0:
+            return False
+
+        # Ignore commands operating on non-source files (e.g. linking)
+        if not codebasin.source.is_source_file(self.filename):
+            return False
+
+        return True
+
+    @classmethod
+    def from_json(cls, instance: dict):
+        """
+        Parameters
+        ----------
+        instance: dict
+            A JSON object representing a single compile command.
+
+        Returns
+        -------
+        CompileCommand
+            A CompileCommand corresponding to the JSON object.
+        """
+        directory = instance["directory"] if "directory" in instance else None
+        arguments = instance["arguments"] if "arguments" in instance else None
+        command = instance["command"] if "command" in instance else None
+        output = instance["output"] if "output" in instance else None
+        return cls(
+            instance["file"],
+            directory=directory,
+            arguments=arguments,
+            command=command,
+            output=output,
+        )
