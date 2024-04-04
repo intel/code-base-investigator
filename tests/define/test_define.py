fefd7d0d1cf92a32e7bc1f5626f19ac4ab59bd0c
John Pennycook <john.pennycook@intel.com>
2024-04-04 10:31:24 -0700

    Rewrite define test without YAML
    Signed-off-by: John Pennycook <john.pennycook@intel.com>

--- tests/define/test_define.py
+++ tests/define/test_define.py
@@ -1,13 +1,15 @@
-# Copyright (C) 2019 Intel Corporation
+# Copyright (C) 2019-2024 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 
-import unittest
 import logging
-from codebasin import config, finder, walkers
+import os
+import unittest
+
+from codebasin import finder
 from codebasin.walkers.platform_mapper import PlatformMapper
 
 
-class TestExampleFile(unittest.TestCase):
+class TestDefine(unittest.TestCase):
     """
     Simple test of ability to recognize #define directives within files.
     """
@@ -16,17 +18,49 @@ class TestExampleFile(unittest.TestCase):
         self.rootdir = "./tests/define/"
         logging.getLogger("codebasin").disabled = True
 
-        self.expected_setmap = {frozenset([]): 4,
-                                frozenset(['CPU', 'GPU']): 10}
+        self.expected_setmap = {
+            frozenset([]): 4,
+            frozenset(["CPU", "GPU"]): 10,
+        }
 
     def test_yaml(self):
         """define/define.yaml"""
-        codebase, configuration = config.load("./tests/define/define.yaml", self.rootdir)
+        codebase = {
+            "files": [
+                os.path.realpath(os.path.join(self.rootdir, "main.cpp")),
+            ],
+            "platforms": ["CPU", "GPU"],
+            "exclude_files": set(),
+            "exclude_patterns": [],
+            "rootdir": self.rootdir,
+        }
+        configuration = {
+            "CPU": [
+                {
+                    "file": codebase["files"][0],
+                    "defines": ["CPU"],
+                    "include_paths": [],
+                    "include_files": [],
+                },
+            ],
+            "GPU": [
+                {
+                    "file": codebase["files"][0],
+                    "defines": ["GPU"],
+                    "include_paths": [],
+                    "include_files": [],
+                },
+            ],
+        }
         state = finder.find(self.rootdir, codebase, configuration)
         mapper = PlatformMapper(codebase)
         setmap = mapper.walk(state)
-        self.assertDictEqual(setmap, self.expected_setmap, "Mismatch in setmap")
+        self.assertDictEqual(
+            setmap,
+            self.expected_setmap,
+            "Mismatch in setmap",
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
