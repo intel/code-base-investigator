5266702debd9093929849db90ecbb4bcb7a89211
John Pennycook <john.pennycook@intel.com>
2024-04-04 09:39:22 -0700

    Rewrite basic Fortran test without YAML
    Signed-off-by: John Pennycook <john.pennycook@intel.com>

--- tests/basic_fortran/test_basic_fortran.py
+++ tests/basic_fortran/test_basic_fortran.py
@@ -1,13 +1,15 @@
-# Copyright (C) 2019 Intel Corporation
+# Copyright (C) 2019-2024 Intel Corporation
 # SPDX-License-Identifier: BSD-3-Clause
 
-import unittest
 import logging
-from codebasin import config, finder, walkers
+import os
+import unittest
+
+from codebasin import finder
 from codebasin.walkers.platform_mapper import PlatformMapper
 
 
-class TestExampleFile(unittest.TestCase):
+class TestBasicFortran(unittest.TestCase):
     """
     Simple test of ability to handle directives in Fortran code.
     """
@@ -16,19 +18,50 @@ class TestExampleFile(unittest.TestCase):
         self.rootdir = "./tests/basic_fortran/"
         logging.getLogger("codebasin").disabled = True
 
-        self.expected_setmap = {frozenset(['CPU']): 2,
-                                frozenset(['GPU']): 3,
-                                frozenset(['CPU', 'GPU']): 8}
+        self.expected_setmap = {
+            frozenset(["CPU"]): 2,
+            frozenset(["GPU"]): 3,
+            frozenset(["CPU", "GPU"]): 8,
+        }
 
     def test_yaml(self):
         """basic_fortran/basic_fortran.yaml"""
-        codebase, configuration = config.load(
-            "./tests/basic_fortran/basic_fortran.yaml", self.rootdir)
+        codebase = {
+            "files": [
+                os.path.realpath(os.path.join(self.rootdir, "test.f90")),
+            ],
+            "platforms": ["CPU", "GPU"],
+            "exclude_files": set(),
+            "exclude_patterns": [],
+            "rootdir": self.rootdir,
+        }
+        configuration = {
+            "CPU": [
+                {
+                    "file": codebase["files"][0],
+                    "defines": ["CPU"],
+                    "include_paths": [],
+                    "include_files": [],
+                },
+            ],
+            "GPU": [
+                {
+                    "file": codebase["files"][0],
+                    "defines": ["GPU"],
+                    "include_paths": [],
+                    "include_files": [],
+                },
+            ],
+        }
         state = finder.find(self.rootdir, codebase, configuration)
         mapper = PlatformMapper(codebase)
         setmap = mapper.walk(state)
-        self.assertDictEqual(setmap, self.expected_setmap, "Mismatch in setmap")
+        self.assertDictEqual(
+            setmap,
+            self.expected_setmap,
+            "Mismatch in setmap",
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
