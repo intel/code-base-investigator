93c6ae4d2f20708d3f868bb86812c0ce485d0913
John Pennycook <john.pennycook@intel.com>
2024-12-16 16:53:32 +0000

    Tweak cli interfaces to make them easier to test
    Signed-off-by: John Pennycook <john.pennycook@intel.com>

--- codebasin/__main__.py
+++ codebasin/__main__.py
@@ -231,7 +231,7 @@ def _main():
     # Generate meta-warnings and statistics.
     # Temporarily override log_level to ensure they are visible.
     stdout_handler.setLevel(logging.WARNING)
-    aggregator.warn()
+    aggregator.warn(log)
     stdout_handler.setLevel(log_level)
 
     # Count lines for platforms
